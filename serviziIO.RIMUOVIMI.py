3ae4c222f88102bb0a3300268f0de2d4e24884f1
Francesco Del Castillo <50611668+franthemanIT@users.noreply.github.com>
2021-11-09 10:56:50 +0100

    Aggiunta gestione dell'ID del servizio IO
    Aggiunta gestione dell'ID del servizio IO. Prima o poi potrebbe servire.

--- serviziIO.RIMUOVIMI.py
+++ serviziIO.RIMUOVIMI.py
@@ -3,12 +3,13 @@
 ##NON USARE LETTERE ACCENTATE E CARATTERI SPECIALI, per scrivere un apostrofo ' fargli precedere una barra \, cioè scrivere \'
 
 
-##per ogni servizio presente su IO aggiungere 5 righe:
+##per ogni servizio presente su IO aggiungere le seguenti righe righe:
 ##serviziIO["CODICE"]={}
 ##serviziIO["CODICE"]["APIKEY"]="APIKEY primaria (da backoffice IO)"
 ##serviziIO["CODICE"]["APIKEY2"]="APIKEY secondaria(da backoffice IO)"
+##serviziIO["CODICE"]["ID"]="ID del servizio in IO"
 ##serviziIO["CODICE"]["nome"]="Descrizione del servizio (come da backoffice IO, ma non fondamentale)"
-##serviziIO["CODICE"]["pagabile"]="Sì"  ##indicare "Sì"/"No" se il servizio è abilitata a veicolare pagamenti (da backoffice IO)
+##serviziIO["CODICE"]["pagabile"]="Sì"  ##indicare "Sì"/"No" se il servizio è abilitato a veicolare pagamenti (da backoffice IO)
 ##serviziIO["CODICE"]["testoFisso"]="No"   ##indicare "Sì"/"No" se di intende utilizzare il servizio IO anche per messaggi con testo fisso senza variabili
 
 
@@ -16,6 +17,7 @@ serviziIO={}
 serviziIO["AP"]={}
 serviziIO["AP"]["APIKEY"]="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 serviziIO["AP"]["APIKEY2"]="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+serviziIO["AP"]["ID"]="xxxxxxxxxxxxxxxxxxxxxxxxxx"
 serviziIO["AP"]["nome"]="Avviso di pagamento"
 serviziIO["AP"]["pagabile"]="Sì"
 serviziIO["AP"]["testoFisso"]="No"
